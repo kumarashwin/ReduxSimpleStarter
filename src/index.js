86af5567a95ad94bfd0346d0b2e26fbdb97d20fd
Ashwin Kumar <askumar@admins-MacBook-Pro.local>
2017-03-15 15:57:21 -0400

    Reverting to clean state

--- src/index.js
+++ src/index.js
@@ -1,48 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import YouTubeSearch from 'youtube-api-search';
-import _ from 'lodash';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-import SearchBar from './components/search-bar';
-import VideoList from './components/video-list';
-import VideoDetail from './components/video-detail';
+import App from './components/app';
+import reducers from './reducers';
 
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-// Youtube API key
-const APIkey = 'AIzaSyAqzmttximCsaCYpkZNutdmBkHVKMrPmJE';
-
-class App extends Component {
-    constructor(props){
-        super(props);
-
-        this.state = { 
-            videos: [],
-            selectedVideo: null  
-        };
-
-        this.videoSearch('surfboards');
-    }
-
-    videoSearch(term) {
-        YouTubeSearch(
-            { key: APIkey, term: term },
-            (videos) => this.setState({
-                videos: videos, 
-                selectedVideo: videos[0]
-            })
-        );
-    }
-
-    render() {
-        return(
-            <div>
-                <SearchBar onSearchTermChange={_.debounce(term => {this.videoSearch(term)}, 300)} />
-                <VideoDetail video={this.state.selectedVideo} />
-                <VideoList videos={this.state.videos} onVideoSelect={ selectedVideo => this.setState({selectedVideo}) }/>
-            </div> 
-        );
-    }
-}
-
-// Put component in DOM
-ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.querySelector('.container'));
