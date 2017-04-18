13cf0f9b6f07a487564103481b521220dd55be70
Ashwin Kumar <kumar.ashwin@outlook.com>
2017-04-18 15:01:00 -0400

    Reverting to clean slate
    This reverts commit 8f45cec990bad69f0579740f9e88044af843b3ca.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,7 @@
 import { combineReducers } from 'redux';
-import weather from './reducer-weather';
 
 const rootReducer = combineReducers({
-    weather
+  state: (state = {}) => state
 });
 
 export default rootReducer;
